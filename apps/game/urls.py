7e439042f19c467e12d5b1d24754cb4871c4be90
serenityjung <wjd7542@cau.ac.kr>
2024-01-18 23:06:01 +0900

    [Add] main, attack, login templates

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
 
-app_name='apps.game'
+app_name='game'
 
-urlpatterns = []
+urlpatterns = [
+    path('',main,name='main'),
+    path('login/', login, name='login'),
+    path('attack/',attack,name='attack'),
+    path('list/', game_list , name='list'),
+#     path('detail/<int:pk>/', detail, name='detail'),
+#     path('defend/<int:pk>/', defend, name="defend"),
+#     path('delete/<int:pk>/', delete, name="delete"),
+#     path('ranking/', ranking, name='ranking'),
+]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+def main(request):
+    return render(request,'game/game_main.html')
+
+def attack(request):
+    return render(request,'game/game_attack.html')
+
+def game_list(request):
+    return render(request, 'game/game_list.html')
+
+def login(request):
+    return render(request, 'game/game_login.html')
 
 # Create your views here.

91a07f80a388f10ab62f1ca680def2c81f7778af
minju <iaki111@naver.com>
2024-01-19 01:27:45 +0900

    [fix] 공격하기

--- apps/game/models.py
+++ apps/game/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-# Create your models here.
+from django.conf import settings
+
 class Game(models.Model):
-    now = models.BooleanField('경기상태')
-    attack_num = models.IntegerField('공격숫자', default = 0,  validators=[MinValueValidator(1), MaxValueValidator(10)])
-    defend_num = models.IntegerField('수비숫자', default = 0,  validators=[MinValueValidator(1), MaxValueValidator(10)])
-    #공격자 나중에 users에서 foreign
-    #수비자 나중에 users에서 foreign
-    attack_user = models.CharField('공격자', max_length = 20)
-    defend_user = models.CharField('수비자', max_length = 20)
+    now = models.BooleanField('경기상태', default=False)
+    attack_num = models.IntegerField('공격숫자', default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    defend_num = models.IntegerField('수비숫자', default=0, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    attack_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='attack_games', verbose_name='공격자')
+    defend_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='defend_games', verbose_name='수비자')
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,21 +1,49 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from .models import Game
+from apps.users.models import User
+from .models import *
 import random
 
 def main(request):
+    if request.method == 'POST':
+        selected_option = request.POST.get('selected_option')
+        selected_user_id = request.POST.get('selected_user')
+        
+        if selected_option and selected_user_id:
+            # 선택한 사용자와 옵션을 사용하여 Game 객체 생성
+            selected_user = User.objects.get(id=selected_user_id)
+            game = Game.objects.create(defend_user=selected_user, attack_num=selected_option, defend_num=1, attack_user=request.user)
+            
+            return redirect('game:attack', pk=game.id)
+    
+    # GET 요청이거나 선택된 값이 없는 경우
+    user = request.user
+    other_users = User.objects.exclude(id=user.id)
     games = Game.objects.all()
     options = random.sample(range(1, 11), 5)
-    ctx = {'games': games, 'options': options}
+    ctx = {'users': other_users, 'options': options, 'games': games}
     return render(request, 'game/game_main.html', ctx)
 
 def attack(request, pk):
     game = Game.objects.get(id=pk)
     ctx = {'game': game}
-    if request.method == 'POST':
-        selected_option = request.POST.get('selected_option', None)
-        
-        if selected_option is not None:
-            game.attack_num = int(selected_option) # 만약 selected_option이 정수라면
-            game.save() # game 객체 업데이트
+    
+    game_number = random.randint(1, 2)
+
+    if game_number == 1:
+        # 게임 1: 수가 클 때 이기는 게임
+        if game.attack_num > game.defend_num:
+            ctx['game_result'] = "수가 클 때 이깁니다! {}승!".format(game.attack_user)
+        else:
+            ctx['game_result'] = "수가 클 때 이깁니다! {}승!".format(game.defend_user)
+    else:
+        # 게임 2: 수가 작을 때 이기는 게임
+        if game.attack_num < game.defend_num:
+            ctx['game_result'] = "수가 작을 때 이깁니다! {}승!".format(game.attack_user)
+        else:
+            ctx['game_result'] = "수가 작을 때 이깁니다! {}승!".format(game.defend_user)
+    
+    game.game_now = True
+    game.save()
+    print(game.game_now)
     return render(request, 'game/game_attack.html', ctx)

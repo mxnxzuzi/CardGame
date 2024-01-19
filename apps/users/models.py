28eb432770a71f0653ee905c1496740fd3ed2a4b
woojin <zaza0804@uos.ac.kr>
2024-01-20 00:29:44 +0900

    feat:닉네임 중복불가 로직

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     points=models.IntegerField(default=0)
-    nickname=models.CharField(max_length=24, null=False)
+    nickname=models.CharField(max_length=24, null=False, unique=True)
 
     def __str__(self):
         return self.nickname

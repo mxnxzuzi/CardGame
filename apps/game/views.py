190529151ef31a6129a1c0a97bb43fff232df0cd
hbam1 <hbbaamm@gmail.com>
2024-01-19 21:29:01 +0900

    [feat] game_detail 및 defend 기능 구현

--- apps/game/views.py
+++ apps/game/views.py
@@ -3,19 +3,70 @@ from django.http import HttpResponseRedirect
 from apps.users.models import User
 from .models import *
 import random
-from .forms import GameForm
-
+from .forms import *
+from django.contrib import auth
 
 def main(request):
     return render(request, 'game/game_main.html')
-def game_list(request):
-    return render(request, 'game/game_list.html')
 
-def detail(request):
-    return render(request, 'game/game_detail.html')
+def defend(request, pk):
+    game = Game.objects.get(id=pk)
+    if request.method == 'GET':
+        counter = request.GET.get('counter', None)
+        form = DefendForm(instance=game)
+        ctx = {
+            'form': form,
+            'pk': pk,
+            'counter': counter
+        }
+        return render(request, 'game/game_defend.html', ctx)
+    form = DefendForm(request.POST, instance=game)
+    if form.is_valid():
+        form.save()
+    game.now = True
+    game.save()
 
-def defend(request):
-    return render(request, 'game/game_defend.html')
+    users = User.objects.all()
+    user_game = Game.objects.get(id=pk)
+    user_nickname = auth.get_user(request).nickname
+    user_num = 0
+    enemy = ''
+    enemy_num = 0
+    winner=''
+
+    if user_game.attack_user.nickname == user_nickname:
+        user_num = user_game.attack_num
+        enemy = user_game.defend_user.nickname
+        enemy_num = user_game.defend_num
+    else:
+        user_num = user_game.defend_num
+        enemy = user_game.attack_user.nickname
+        enemy_num = user_game.attack_num
+
+    if user_game.game_num == 1:
+        if user_num < enemy_num:
+            winner = user_nickname
+        elif user_num > enemy_num:
+            winner = enemy
+    else:
+        if user_num < enemy_num:
+            winner = enemy
+        elif user_num > enemy_num:
+            winner = user_nickname
+
+    me = users.get(nickname=user_nickname)
+    other = users.get(nickname=enemy)
+    if winner == user_nickname:
+        me.points += user_num
+        other.points -= enemy_num
+    elif winner == enemy:
+        me.points -= user_num
+        other.points += enemy_num
+
+    me.save()
+    other.save()
+
+    return redirect(f'../detail/{pk}')
 
 def attack(request):
     return render(request, 'game/game_attacking.html')
@@ -25,7 +76,7 @@ def attack_choice(request):
         form = GameForm(request.POST, current_user=request.user)
         if form.is_valid():
             game = form.save(commit=False)
-            game.attack_user = request.user 
+            game.attack_user = request.user
             game.save()
             return redirect('game:attacking', pk=game.id)
     else:
@@ -62,3 +113,58 @@ def attacking(request, pk):
     game.save()
     return render(request, 'game/game_attacking.html', ctx)
 
+def game_list(request):
+    games = Game.objects.all()
+    user_id = auth.get_user(request).id
+    user_games = games.filter(models.Q(attack_user=user_id) | models.Q(defend_user=user_id))
+    user_games = user_games.order_by("-id")
+    ctx = {
+        'user_games': user_games
+    }
+    return render(request, 'game/game_list.html', ctx)
+
+def game_detail(request, pk):
+    users = User.objects.all()
+    user_game = Game.objects.get(id=pk)
+    user_nickname = auth.get_user(request).nickname
+    counter = request.GET.get('counter', None)
+    user_num = 0
+    enemy = ''
+    enemy_num = 0
+    winner=''
+
+    if user_game.attack_user.nickname == user_nickname:
+        user_num = user_game.attack_num
+        enemy = user_game.defend_user.nickname
+        enemy_num = user_game.defend_num
+    else:
+        user_num = user_game.defend_num
+        enemy = user_game.attack_user.nickname
+        enemy_num = user_game.attack_num
+
+    if user_game.game_num == 1:
+        if user_num < enemy_num:
+            winner = user_nickname
+        elif user_num > enemy_num:
+            winner = enemy
+    else:
+        if user_num < enemy_num:
+            winner = enemy
+        elif user_num > enemy_num:
+            winner = user_nickname
+
+    ctx = {
+        'user_game': user_game,
+        'counter': counter,
+        'user_num': user_num,
+        'enemy': enemy,
+        'enemy_num': enemy_num,
+        'winner': winner
+    }
+    return render(request, 'game/game_detail.html', ctx)
+
+def game_delete(request, pk):
+    if request.method == 'POST':
+        user_game = Game.objects.get(id=pk)
+        user_game.delete()
+    return redirect("game:list")
